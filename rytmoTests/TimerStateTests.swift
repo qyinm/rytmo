6c4ab97b8a283b5fe4952522a51a2017394d4a67
qyinm <qusseun@gmail.com>
2026-01-15 11:38:51 +0900

    test: add TimerState unit tests

---
+++ rytmoTests/TimerStateTests.swift
@@ -0,0 +1,48 @@
+//
+//  TimerStateTests.swift
+//  rytmoTests
+//
+//  Unit tests for TimerState enum
+//
+
+import XCTest
+@testable import rytmo
+
+final class TimerStateTests: XCTestCase {
+    
+    // MARK: - displayName Tests
+    
+    func testDisplayName_Idle() {
+        XCTAssertEqual(TimerState.idle.displayName, "Idle")
+    }
+    
+    func testDisplayName_Focus() {
+        XCTAssertEqual(TimerState.focus.displayName, "Focus")
+    }
+    
+    func testDisplayName_ShortBreak() {
+        XCTAssertEqual(TimerState.shortBreak.displayName, "Short Break")
+    }
+    
+    func testDisplayName_LongBreak() {
+        XCTAssertEqual(TimerState.longBreak.displayName, "Long Break")
+    }
+    
+    // MARK: - defaultDuration Tests
+    
+    func testDefaultDuration_Idle() {
+        XCTAssertEqual(TimerState.idle.defaultDuration, 0)
+    }
+    
+    func testDefaultDuration_Focus() {
+        XCTAssertEqual(TimerState.focus.defaultDuration, 25 * 60)
+    }
+    
+    func testDefaultDuration_ShortBreak() {
+        XCTAssertEqual(TimerState.shortBreak.defaultDuration, 5 * 60)
+    }
+    
+    func testDefaultDuration_LongBreak() {
+        XCTAssertEqual(TimerState.longBreak.defaultDuration, 15 * 60)
+    }
+}
